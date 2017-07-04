e89e61fd34ed69ed5f9e1a4eafd0f308e5a350d1
Rails Udemy Course <railsudemycourse@gmail.com>
2017-07-04 14:27:30 +0000

    Add partials and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,14 @@ class ArticlesController < ApplicationController
   
   def show
     @article =Article.find(params[:id])
-  end  
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Articles was successfully deleted"
+    redirect_to articles_path
+  end
   
   private
     def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'welcome/home', to:'welcome#home' # get to 'welcome' controller and get 'home'
+  root 'welcome#home' # get to 'welcome' controller and get 'home'
   get 'welcome/about', to:'welcome#about'
   
   resources :articles
