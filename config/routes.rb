5dafee184cf7355b6daa366c2264d20784245333
Rails Udemy Course <railsudemycourse@gmail.com>
2017-08-11 09:21:16 +0000

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   #post 'users', to: 'users#create'
   resources :users, except: [:new]
+  
+  resources :categories, except: [:destroy]
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
